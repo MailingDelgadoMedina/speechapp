8911eba89e33c5527513db8f7705fee842747189
MailingDelgadoMedina <mailing.delgado.scitexas@gmail.com>
2022-08-31 01:55:16 -0400

    Boiler working perfectly. Happy coding!

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+
+
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-     <h1>Coaching Call Speech App</h1>
+    <div >
+
+     <h1 className='text-4xl'>Coaching Call Speech App</h1>
 
     </div>
   )
